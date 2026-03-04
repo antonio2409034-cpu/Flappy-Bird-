3de7d0e513363eae38ad10f2836f9f520cd217e4
OnlyWicho <emilnmga@gmail.com>
2026-03-04 14:37:05 -0600

    added files are code for the game...

---
+++ FlappyBirdCLI/src/main/java/com/mycompany/flappybirdcli/FlappyBirdCLI.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.flappybirdcli;
+import java.util.*;
+/**
+ *
+ * @author Student
+ */
+public class FlappyBirdCLI {
+
+    static final int HEIGHT = 20;
+    static final int WIDTH = 40;
+    static final int BIRD_X = 10;
+    
+    static int birdY = HEIGHT / 2;
+    static int velocity = 0;
+    static int gravity = 1;
+    static int jumpStrength = -3;
+    
+    static int pipeX = WIDTH - 1;
+    static int gapY = 8;
+    static int gapSize = 5;
+
+    static boolean gameOver = false;
+    static int score = 0;
+    
+    Public static void main(String[] args) throws Exception{
+        
+        
+    }
+  
+}
